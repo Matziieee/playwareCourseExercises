8560910cba34edf1e94fe3c2cffa414d8a563571
Mads Kristian Rosendal Højberg Bau-Madsen <mkbm@netcompany.com>
2021-02-25 13:18:11 +0100

    add initial logic for special one game and activity

--- app/src/main/java/com/playware/exercise2/SpecialOneActivity.java
+++ app/src/main/java/com/playware/exercise2/SpecialOneActivity.java
@@ -3,12 +3,69 @@ package com.playware.exercise2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
-public class SpecialOneActivity extends AppCompatActivity {
+import com.livelife.motolibrary.GameType;
+import com.livelife.motolibrary.MotoConnection;
+import com.livelife.motolibrary.MotoSound;
+import com.livelife.motolibrary.OnAntEventListener;
+
+public class SpecialOneActivity extends AppCompatActivity implements OnAntEventListener {
+    MotoConnection connection = MotoConnection.getInstance();
+    MotoSound motoSound = MotoSound.getInstance();
+    LinearLayout gameTypeContainer;
+    SpecialOneGame game;
+
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        game.stopGame();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_special_one);
+
+        connection.registerListener(this);
+        connection.setAllTilesToInit();
+
+        game = new SpecialOneGame();
+        gameTypeContainer = findViewById(R.id.specialOneLayout);
+
+        for (final GameType gt : game.getGameTypes()) {
+            Button b = new Button(this);
+            b.setText(gt.getName());
+            b.setOnClickListener(v -> {
+                motoSound.playStart();
+                game.selectedGameType = gt;
+                game.startGame();
+            });
+            gameTypeContainer.addView(b);
+        }
+    }
+
+    @Override
+    public void onMessageReceived(byte[] bytes, long l) {
+        game.addEvent(bytes);
+    }
+
+    @Override
+    public void onAntServiceConnected() {
+
+    }
+
+    @Override
+    public void onNumbersOfTilesConnected(int i) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        connection.stopMotoConnection();
+        connection.unregisterListener(this);
     }
 }
---
+++ app/src/main/java/com/playware/exercise2/SpecialOneGame.java
@@ -0,0 +1,93 @@
+package com.playware.exercise2;
+
+import com.livelife.motolibrary.AntData;
+import com.livelife.motolibrary.Game;
+import com.livelife.motolibrary.GameType;
+import com.livelife.motolibrary.MotoConnection;
+import com.livelife.motolibrary.MotoSound;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+import static com.livelife.motolibrary.AntData.*;
+
+public class SpecialOneGame extends Game {
+
+    MotoConnection connection = MotoConnection.getInstance();
+    MotoSound motoSound = MotoSound.getInstance();
+
+    ArrayList<Integer> colours = new ArrayList<>();
+    Random r = new Random(1);
+    int currentSpecialTile = -1;
+    int score = 0;
+
+    public SpecialOneGame() {
+        setName("Special One");
+        setDescription("Another cool game");
+
+        GameType gt = new GameType(1, GameType.GAME_TYPE_TIME, 30,"1 player 30 sec",1);
+        addGameType(gt);
+
+        GameType gt2 = new GameType(2, GameType.GAME_TYPE_TIME, 60,"1 player 1 min",1);
+        addGameType(gt2);
+        addColours();
+    }
+
+    @Override
+    public void onGameStart() {
+        super.onGameStart();
+        progressGame();
+    }
+
+    private void progressGame(){
+        connection.setAllTilesIdle(LED_COLOR_OFF);
+        int tile = connection.randomIdleTile();
+        this.currentSpecialTile = tile;
+        int specialColor = getRandomColor();
+        int restColor = getRandomColor(specialColor);
+        for (int i : connection.connectedTiles) {
+            if(i == tile){
+                connection.setTileColor(specialColor,i);
+            }else{
+                connection.setTileColor(restColor, i);
+            }
+        }
+    }
+
+    private void addColours(){
+        colours.add(LED_COLOR_BLUE);
+        colours.add(LED_COLOR_ORANGE);
+        colours.add(LED_COLOR_GREEN);
+        colours.add(LED_COLOR_RED);
+        colours.add(LED_COLOR_VIOLET);
+        colours.add(LED_COLOR_INDIGO);
+    }
+
+    private int getRandomColor(){
+       return getRandomColor(-1);
+    }
+
+    private int getRandomColor(int ignore){
+        int color = ignore;
+        while(color == ignore){
+            color = this.colours.get(r.nextInt(this.colours.size()));
+        }
+        return color;
+    }
+
+    @Override
+    public void onGameUpdate(byte[] message) {
+        super.onGameUpdate(message);
+        int event = AntData.getCommand(message);
+        int id = AntData.getId(message);
+        if(event == EVENT_PRESS && id == this.currentSpecialTile){
+            this.score++;
+            progressGame();
+        }
+    }
+
+    @Override
+    public void onGameEnd() {
+        super.onGameEnd();
+    }
+}
